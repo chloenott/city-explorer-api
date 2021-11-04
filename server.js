66b8ddf94d0271cb6e14958662b812dcdeca9299
Chloe Nott <chloe@nott.email>
2021-11-03 19:58:05 -0700

    Renamed property names to match api's

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ class Forecast {
 }
 
 class Movie {
-    constructor({title, overview, average_votes, total_votes, image_url, popularity, released_on}) {
+    constructor({title, overview, vote_average, vote_count, original_language, popularity, released_date}) {
         this.title = title;
         this.overview = overview;
-        this.average_votes = average_votes;
-        this.total_votes = total_votes;
-        this.image_url = image_url;
+        this.vote_average = vote_average;
+        this.vote_count = vote_count;
+        this.original_language = original_language;
         this.popularity = popularity;
-        this.released_on = released_on;
+        this.released_date = released_date;
     }
 }
 
