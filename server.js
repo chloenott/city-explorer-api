2f59cd75ba8914d68b12584a0cc16603ed8b4451
Chloe Nott <chloe@nott.email>
2021-11-03 17:05:13 -0700

    Added catch to handleWeather

--- server.js
+++ server.js
@@ -14,21 +14,26 @@ class Forecast {
 }
 
 function handleWeather(request, response) {
-    let input = {
-        lat: Number(request.query.lat),
-        lon: Number(request.query.lon),
-        searchQuery: request.query.searchQuery || ''
+    try {
+        let input = {
+            lat: request.query.lat,
+            lon: request.query.lon,
+            searchQuery: request.query.searchQuery || ''
+        }
+
+        let weatherAtCity = weather.find( weatherObj => findCity(weatherObj, input) );
+
+        let forecasts = weatherAtCity && weatherAtCity.data.map( forecast => {
+            let date = forecast.datetime;
+            let description = `Low of ${forecast.low_temp}, high of ${forecast.high_temp} with ${forecast.weather.description}`;
+            return new Forecast(date, description);
+        });
+
+        return weatherAtCity ? response.status(200).send(forecasts) : response.status(400).send('Unable to find weather data.');
+
+    } catch {
+        return response.status(500).send('Internal server error.')
     }
-
-    let weatherAtCity = weather.find( weatherObj => findCity(weatherObj, input) );
-
-    let forecasts = weatherAtCity && weatherAtCity.data.map( forecast => {
-        let date = forecast.datetime;
-        let description = `Low of ${forecast.low_temp}, high of ${forecast.high_temp} with ${forecast.weather.description}`;
-        return new Forecast(date, description);
-    });
-
-    return weatherAtCity ? response.status(200).send(forecasts) : response.status(400).send('Unable to find weather data.');
 }
 
 function findCity(weatherObj, input) {
@@ -40,13 +45,13 @@ function findByCityName(weatherObj, cityName) {
 }
 
 function findByCoordinates(weatherObj, lat, lon) {
-    let checkLat = Math.abs(Number(weatherObj.lat) - lat) < 0.1
-    let checkLon = Math.abs(Number(weatherObj.lon) - lon) < 0.1;
+    let checkLat = Math.abs(Number(weatherObj.lat) - Number(lat)) < 0.1
+    let checkLon = Math.abs(Number(weatherObj.lon) - Number(lon)) < 0.1;
     return checkLat && checkLon;
 }
 
 function handleError(request, response) {
-    response.status(500).send('Not found.')
+    response.status(404).send('Not found.')
 }
 
 app.use(cors())
