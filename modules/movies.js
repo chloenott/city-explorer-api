65ae0334b17f240ea4595239de931acae67d0980
Chloe Nott <chloe@nott.email>
2021-11-08 01:11:14 -0800

    Updated names to match what they were before for front end compatibility

--- modules/movies.js
+++ modules/movies.js
@@ -6,9 +6,9 @@ const { response } = require('express');
 
 module.exports = getMovie;
 
-function getMovie(input) {
-  const key = 'movie-' + input;
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${input}&page=1&include_adult=false`;
+function getMovie(city) {
+  const key = 'movie-' + city;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${city}&page=1&include_adult=false`;
 
   if (cache[key] && (Date.now() - cache[key].timestamp < 1000*60*60*24)) {
     console.log('Cache hit');
